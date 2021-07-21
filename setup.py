bcf28b446a41bf436e8f5d862973632a7c77ab7d
Anssi Törmä <anssi.torma@iki.fi>
2021-07-21 09:02:10 +0300

    Fix wrong url in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Zyxel NR7101 InfluxDB Collector',
     author='Anssi Törmä',
     license='MIT',
-    url='https://github.com/atorma/nr7101',
+    url='https://github.com/atorma/nr7101-influxdb-collector',
     packages=find_packages(),
     install_requires=[
         'ping3',
