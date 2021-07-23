13dadf742f335e4a11984101dd6980b6f8fbb00d
Anssi Törmä <anssi.torma@iki.fi>
2021-07-23 10:59:22 +0300

    Use InfluxDB v2 for MacOS M1 with InfluxdDB V1 compatibility

---
+++ services/setup-v1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+influx v1 dbrp create \
+  --bucket-id ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+  --db ${V1_DB_NAME} \
+  --rp ${V1_RP_NAME} \
+  --default \
+  --org ${DOCKER_INFLUXDB_INIT_ORG}
+
+influx v1 auth create \
+  --username ${V1_AUTH_USERNAME} \
+  --password ${V1_AUTH_PASSWORD} \
+  --read-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+  --write-bucket ${DOCKER_INFLUXDB_INIT_BUCKET_ID} \
+  --org ${DOCKER_INFLUXDB_INIT_ORG}
